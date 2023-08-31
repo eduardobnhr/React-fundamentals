0e897b77574050a744edf2a388528eed60409b99
eduardo <eduardobnhr@gmail.com>
2023-08-31 09:06:13 -0400

    create project e components

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+export function Home() {
+
+  return (
+    <div className='container'>
+      <h1>Lista de presenca</h1>
+      <input type="text" placeholder="digite um nome"/>
+      <button type="button">Adicionar</button>
+    </div>
+  )
+}
+
+
